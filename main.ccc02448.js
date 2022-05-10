76626d00d5a229a88408c32025696688686e9542
MariaBondar <marybondar777@gmail.com>
2022-05-10 15:23:20 +0200

    make build

---
+++ main.ccc02448.js
@@ -0,0 +1,10 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"sFVd":[function(require,module,exports) {
+module.exports="Photo.904e5c3a.png";
+},{}],"aep9":[function(require,module,exports) {
+module.exports="slide-img-1.0d321768.png";
+},{}],"cKVn":[function(require,module,exports) {
+module.exports="slide-img-2.74a17766.png";
+},{}],"KIzB":[function(require,module,exports) {
+"use strict";var e=n(require("../images/Photo.png")),t=n(require("../images/slider/slide-img-1.png")),r=n(require("../images/slider/slide-img-2.png"));function n(e){return e&&e.__esModule?e:{default:e}}var u=[e.default,t.default,r.default],o=0,c=document.querySelector(".header__image"),d=document.querySelector(".button--next"),a=document.querySelector(".button--prev");d.addEventListener("click",function(){++o>=3&&(o=0);u[o];c.style.backgroundImage="url('".concat(u[o],"')"),console.log(document.body.style.backgroundImage)}),a.addEventListener("click",function(){--o<=3&&(o=0);u[o];c.style.backgroundImage="url('".concat(u[o],"')")});
+},{"../images/Photo.png":"sFVd","../images/slider/slide-img-1.png":"aep9","../images/slider/slide-img-2.png":"cKVn"}]},{},["KIzB"], null)
+//# sourceMappingURL=main.ccc02448.js.map
